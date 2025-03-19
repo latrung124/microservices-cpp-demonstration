a97a82af3df60461cbf1a40828f7676bb19e8a23
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-03-19 23:31:14 +0700

    Add yaml-cpp and httplib-cpp package

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,8 @@ class ExampleRecipe(ConanFile):
 
     def requirements(self):
         self.requires("protobuf/5.27.0")
+        self.requires("yaml-cpp/0.8.0")
+        self.requires("cpp-httplib/0.18.0")
     
     def build_requirements(self):
         self.build_requires("cmake/3.22.6")
