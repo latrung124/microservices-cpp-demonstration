c82b0ab396831925a5fe81e7fc69a260244a0fcd
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-03-20 23:59:03 +0700

    Add yaml config file and init the env variable sample

--- common/config_manager.cpp
+++ common/config_manager.cpp
@@ -7,10 +7,18 @@
 
 #include "config_manager.h"
 
+#include <yaml-cpp/yaml.h>
+
 ConfigManager::ConfigManager(std::string_view configPath)
 {
     // Load configuration from yaml file
-    // ...
+    YAML::Node config = YAML::LoadFile(std::string(configPath));
+    m_env = config["environment"].as<std::string>();
+
+    for (const auto &service : config["services"]) {
+        m_services[service.first.as<std::string>()] =
+        m_env == "cloud" ? service.second["cloud"].as<std::string>() : service.second["local"].as<std::string>();
+    }
 }
 
 std::string ConfigManager::getServiceUrl(const std::string &serviceName) const
